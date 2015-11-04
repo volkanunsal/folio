32e6bbc1716d12c5c0b2972957064c6517360bca
Volkan Unsal <spocksplanet@gmail.com>
2015-11-04 18:14:53 -0500

    Add export declarations for adapters

---
+++ src/adapters/index.js
@@ -0,0 +1,9 @@
+import LCircle from './L/Circle';
+import LCircleMarker from './L/CircleMarker';
+import LControl from './L/Control';
+import LFeature from './L/Feature';
+import LMap from './L/Map';
+import LMarker from './L/Marker';
+import LTile from './L/Tile';
+
+export default { LCircle, LCircleMarker, LControl, LFeature, LMap, LMarker, LTile };
